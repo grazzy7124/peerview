5e754bbbe3c479504ae366334db280d425b27ddb
MunHayung <22500244@handong.ac.kr>
2026-01-12 16:58:10 +0900

    수업 선텍 / 설문 생성 UI

--- lib/core/widgets/class_chip.dart
+++ lib/core/widgets/class_chip.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
 
 class ClassChip extends StatelessWidget {
+  final bool isSelected;
+  final VoidCallback onTap;
+  final String title;
+
   const ClassChip({
     super.key,
+    required this.isSelected,
+    required this.onTap,
+    required this.title,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 320, height: 55,
-      margin: EdgeInsets.only(bottom: 12),
-      decoration: BoxDecoration(
-        color: Color(0xffFAFAFA),
-        border: BoxBorder.all(
-          color: Color(0xffCDCDCD),
-          width: 1
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 320,
+        height: 55,
+        margin: const EdgeInsets.only(bottom: 12),
+        decoration: BoxDecoration(
+          color: isSelected
+            ? const Color(0xffE0EAFF)
+            : const Color(0xffFAFAFA),
+          border: Border.all(
+            color: isSelected
+              ? const Color(0xff164BC6)
+              : const Color(0xffCDCDCD),
+            width: isSelected 
+              ? 2 
+              : 1,
+          ),
+          borderRadius: BorderRadius.circular(100),
         ),
-        borderRadius: BorderRadius.circular(100)
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 15, right: 15),
-        child: Row(
-          children: [
-            Container(
-              width: 25, height: 25,
-              decoration: BoxDecoration(
-                color: Color(0xffD9D9D9),
-                borderRadius: BorderRadius.circular(100)
-              ),
-              child: Center(
-                child: SizedBox(
-                  width: 14,
-                  child: Image.asset(
-                    'assets/images/check_icon.png'
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            children: [
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  color: isSelected
+                    ? const Color(0xff164BC6)
+                    : const Color(0xffD9D9D9),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Center(
+                  child: SizedBox(
+                    width: 14,
+                    child: Image.asset('assets/images/check_icon.png'),
                   ),
                 ),
-              )
-            ),
-            SizedBox(width: 10,),
-            Text(
-              '공학설계입문',
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontSize: 16,
-                fontWeight: FontWeight.w600
               ),
-            )
-          ],
+              SizedBox(width: 10),
+              Text(title, style: const TextStyle( fontFamily: 'Pretendard', fontSize: 16, fontWeight: FontWeight.w600,),),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/survey/survey_form_page.dart
+++ lib/views/survey/survey_form_page.dart
@@ -1,10 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:peerview/core/widgets/forms.dart';
 
-class SurveyFormPage extends StatelessWidget {
+class SurveyFormPage extends StatefulWidget {
   const SurveyFormPage({super.key});
 
   @override
+  State<SurveyFormPage> createState() => _SurveyFormPageState();
+}
+
+class _SurveyFormPageState extends State<SurveyFormPage> {
+  final TextEditingController _surveyTitleController = TextEditingController();
+  final TextEditingController _surveyDateController = TextEditingController();
+
+  bool get _isFormComplete =>
+      _surveyTitleController.text.isNotEmpty &&
+      _surveyDateController.text.isNotEmpty;
+
+  @override
+  void initState() {
+    super.initState();
+    _surveyTitleController.addListener(_onChanged);
+    _surveyDateController.addListener(_onChanged);
+  }
+
+  void _onChanged() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _surveyTitleController.dispose();
+    _surveyDateController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              TextField(
+                controller: _surveyTitleController,
+                decoration: InputDecoration(
+                  suffixIcon: Image.asset('assets/images/form_title_edit_icon.png', scale: 2.8,),
+                  hintText: "제목 없는 평가지",
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xffE5E7EB), 
+                      width: 1.96,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff164BC6),
+                      width: 1.96,
+                    ),
+                  ),
+                ),
+                
+                onChanged: (value) {
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              Text('마감일을 설정하세요.', style: TextStyle(color: Color(0xff333333)),),
+              TextField(
+                controller: _surveyDateController,
+                decoration: InputDecoration(
+                  suffixIcon: Image.asset('assets/images/form_calendar_icon.png', scale: 2.8,),
+                  hintText: "2026.01.01",
+                  hintStyle: TextStyle(
+                    color: Color(0xff9CA3AF),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xffE5E7EB), 
+                      width: 1.96,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xff164BC6),
+                      width: 1.96,
+                    ),
+                  ),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              Forms(),
+              SizedBox(height: 19,),
+              GestureDetector(
+                onTap: (){
+
+                },
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Color(0xff2A71FF), Color(0xff164BC6)]
+                      ),
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: Image.asset('assets/images/form_add_btn_icon.png', height: 28,),
+                    )
+                  ),
+                ),
+              ),
+              SizedBox(height: 119,),
+              
+            ]
+          )
+        )
+      )
+    );
   }
 }
